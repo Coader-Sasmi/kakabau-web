2e6ed83c272150a8680d08af6047f1cb3a71c29f
itishree <itishrees977@gmail.com>
2025-04-08 11:36:26 +0530

    initial changes

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -10,15 +10,3 @@ export type navArrType={
     url:string
     submenu?: SubmenuItem[];
 }
-export type VenderIdType={
-    id:"string"
-}
-export type VenderTableArrType ={
-    id:number
-    name:string
-    phone: string
-    email : string
-}
-export type DetailsArrType={
-    id:string
-}
